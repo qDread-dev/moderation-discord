36b56ac47696bb52a3f6a39a220f2625395276a3
qDread-dev <58754377+qDread-dev@users.noreply.github.com>
2022-10-11 13:20:17 -0400

    Ping pong script

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import discord
 
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print("Logged in as ", self.user)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+intents = discord.Intents.default()
+intent.message_content = True
+client MyClient intents=intents
+client.run('token')
