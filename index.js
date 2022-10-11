b2887c86e8ce05a20418b918f9d299ca3018538c
qDread-dev <58754377+qDread-dev@users.noreply.github.com>
2022-10-11 14:40:50 -0400

    Update

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Require the necessary discord.js classes
+const fs = require('node:fs');
+const path = require('node:path');
 const { Client, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
+client.commands = new Collection();
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
